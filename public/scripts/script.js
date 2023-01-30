46dcc075a2c7f335c2dde33d2e9736eefdfa09f6
caitreid <ctlnrd@gmail.com>
2023-01-30 16:40:59 -0500

    wrap functions in ifs to check if element exists before running

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -6,21 +6,30 @@ window.addEventListener("load", () => {
 });
 
 
+// auto-set the date to today on forms
+let datePicker = document.getElementById('datePicker');
 
-document.getElementById('datePicker').valueAsDate = new Date();
+if (datePicker) {
+    document.getElementById('datePicker').valueAsDate = new Date();
+}
 
+// modal 
 let create = document.querySelector('.create')
 let modal = document.querySelector('.modal__card')
 
-create.addEventListener('click', () => {
-    console.log('create')
-    modal.classList.remove('hide')
-    modal.classList.add('show')
-})
+if (create) {
+
+    create.addEventListener('click', () => {
+        console.log('create')
+        modal.classList.remove('hide')
+        modal.classList.add('show')
+    })
+}
+
 
 // let body = document.querySelector('body')
 
-
+// I don't think this is going to work
 document.addEventListener('click', function handleClick(event) {
     const hasClass = event.target.classList.contains('container-fluid');
     console.log(hasClass);
