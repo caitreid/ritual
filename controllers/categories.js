1f2433dc617df78d88832a366016ec167a24a9a4
caitreid <ctlnrd@gmail.com>
2023-01-30 16:42:38 -0500

    categories controller

--- controllers/categories.js
+++ controllers/categories.js
@@ -8,12 +8,8 @@ const router = express.Router()
 // index ALL events
 router.get('/', (req, res) => {
 	Category.find({})
-		// .populate('project', 'title')
-        // .populate('category', 'title')
 
 		.then(categories => {
-			// const username = req.session.username
-			// const loggedIn = req.session.loggedIn
 			
 			res.render('categories/index', { categories })
 		})
