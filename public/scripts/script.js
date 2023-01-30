e92ab33d66ff135409e69672620927b113ac749c
caitreid <ctlnrd@gmail.com>
2023-01-30 04:12:08 -0500

    wip create new event modal

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,8 +1,30 @@
 
-// window.addEventListener("load", () => {
-//     console.log("page is fully loaded");
-//     document.getElementById('datePicker').valueAsDate = new Date();
-//   });
-// alert('hello!')
+window.addEventListener("load", () => {
+    console.log("page is fully loaded"); 
 
-$('.dropdown-toggle').dropdown()
+    
+});
+
+
+
+document.getElementById('datePicker').valueAsDate = new Date();
+
+let create = document.querySelector('.create')
+let modal = document.querySelector('.modal__create')
+
+create.addEventListener('click', () => {
+    console.log('create')
+    modal.classList.remove('hide')
+    modal.classList.add('show')
+})
+
+let body = document.querySelector('body')
+
+body.addEventListener('click', (event) => {
+
+    if((event.target).closest('.container-fluid') && (modal.classList.contains('show')) ) {
+        console.log('outside')
+        modal.classList.remove('show')
+        modal.classList.add('hide')
+    } 
+})
